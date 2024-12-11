264419cbb878b297c66c3d55f0c79a1d086e9546
fyInALT <fy@altresear.ch>
2024-12-11 10:52:49 +0800

    add twitter client

--- packages/plugin-twitter/src/services/twitter-fetcher.ts
+++ packages/plugin-twitter/src/services/twitter-fetcher.ts
@@ -1,7 +1,10 @@
-import {IAgentRuntime, ServiceType} from "@ai16z/eliza";
+import {IAgentRuntime} from "@ai16z/eliza";
 import {Service} from "@ai16z/eliza";
-import {elizaLogger} from "@ai16z/eliza";
+import {elizaLogger, parseBooleanFromText} from "@ai16z/eliza";
+import { Tweet } from "agent-twitter-client";
 import {z} from "zod";
+
+import { ClientBase } from "./base";
 
 async function getTwitterSettings(runtime: IAgentRuntime) {
 
@@ -48,14 +51,82 @@ async function getTwitterSettings(runtime: IAgentRuntime) {
 }
 
 export class TwitterFetcherService extends Service {
-    static serviceType: ServiceType = ServiceType.SPEECH_GENERATION;
+    client: ClientBase;
+    runtime: IAgentRuntime;
 
     async initialize(runtime: IAgentRuntime): Promise<void> {
         const config = await getTwitterSettings(runtime)
         elizaLogger.info("initialize TwitterFetcherService:", config);
+
+        this.client = new ClientBase(runtime);
+        this.runtime = runtime;
+
+        await this.start();
     }
 
     getInstance(): TwitterFetcherService {
         return TwitterFetcherService.getInstance();
+    }
+
+    async start(postImmediately: boolean = false) {
+        if (!this.client.profile) {
+            await this.client.init();
+        }
+
+        const fetchNewTweetLoop = async () => {
+            elizaLogger.info("fetchNewTweetLoop tick");
+
+            const lastPost = await this.runtime.cacheManager.get<{
+                timestamp: number;
+            }>(
+                "twitter/" +
+                    this.runtime.getSetting("TWITTER_USERNAME") +
+                    "/lastPost"
+            );
+
+            const lastPostTimestamp = lastPost?.timestamp ?? 0;
+            const minMinutes =
+                parseInt(this.runtime.getSetting("POST_INTERVAL_MIN")) || 90;
+            const maxMinutes =
+                parseInt(this.runtime.getSetting("POST_INTERVAL_MAX")) || 180;
+            const randomMinutes =
+                Math.floor(Math.random() * (maxMinutes - minMinutes + 1)) +
+                minMinutes;
+            const delay = randomMinutes * 60 * 1000;
+
+            if (Date.now() > lastPostTimestamp + delay) {
+                await this.fetchNewTweet();
+            }
+
+            setTimeout(() => {
+                fetchNewTweetLoop(); // Set up next iteration
+            }, delay);
+
+            elizaLogger.log(`Next tweet scheduled in ${randomMinutes} minutes`);
+        };
+        if (
+            this.runtime.getSetting("POST_IMMEDIATELY") != null &&
+            this.runtime.getSetting("POST_IMMEDIATELY") != ""
+        ) {
+            postImmediately = parseBooleanFromText(
+                this.runtime.getSetting("POST_IMMEDIATELY")
+            );
+        }
+        if (postImmediately) {
+            this.fetchNewTweet();
+        }
+
+        fetchNewTweetLoop();
+    }
+
+    private async fetchNewTweet() {
+        elizaLogger.log("New fetch new twitter");
+
+        // TODO set tweet to knownledge
+        const tweet = await this.client.getCachedTimeline()
+
+        tweet?.forEach((tweet: Tweet) => {
+            elizaLogger.info("got tweet", tweet)
+        })
     }
 }
